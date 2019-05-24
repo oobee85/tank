95499248caf10e533e550183e8b725c7589a7a22
oobee85 <oobee85@gmail.com>
2019-05-24 13:55:45 -0700

    Add files via upload

---
+++ src/Spawn.java
@@ -0,0 +1,17 @@
+import java.awt.Point;
+
+public class Spawn {
+	
+	private Point point;
+	
+	public Spawn(int x, int y) {
+		
+		point = new Point(x, y);
+		
+	}
+	
+	public Point getPoint() {
+		return point;
+	}
+	
+}
